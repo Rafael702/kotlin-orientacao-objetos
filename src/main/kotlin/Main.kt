338f27fbed200f8ca94cfc0be9a9ca0d6e7fa902
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-17 23:01:27 -0300

    * Condicionais no kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,23 @@ fun main(args: Array<String>) {
     val numeroDaConta = 1000
     var saldo = 0.0
     saldo = 100 + 2.0
-    saldo += 200
+    saldo -= 200
 
     println("titular: $titular")
     println("numero da conta: $numeroDaConta")
     println("Saldo da conta: $saldo")
+
+//    if (saldo > 0.0) {
+//        println("conta é positiva")
+//    } else if (saldo == 0.0) {
+//        println("conta é neutra")
+//    } else {
+//        println("conta é negativa")
+//    }
+
+    when {
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
+    }
 }
