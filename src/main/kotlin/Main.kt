20f42670a6a7bf454df6b8869b5a71b85fcb8fe4
Rafael <rafaeldealmeida.192001@gmail.com>
2023-01-19 22:52:50 -0300

    * Loop no kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -19,9 +19,43 @@ fun main(args: Array<String>) {
 //        println("conta é negativa")
 //    }
 
-    when {
-        saldo > 0.0 -> println("conta é positiva")
-        saldo == 0.0 -> println("conta é neutra")
-        else -> println("conta é negativa")
+    testaCondicoes(saldo)
+
+//    when {
+//        saldo > 0.0 -> println("conta é positiva")
+//        saldo == 0.0 -> println("conta é neutra")
+//        else -> println("conta é negativa")
+//    }
+
+//    for (i in 5..1) {
+//        println(i)
+//    }
+
+//    for (i in 5 downTo 1) {
+//        println(i)
+//    }
+
+//    for (i in 1..5 step 2) {
+//        println(i)
+//    }
+
+    for (i in 1 until 5) {
+        println(i)
+    }
+
+//    var i: Int = 1
+//    while (i <= 5) {
+//        println(i)
+//        i++
+//    }
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
     }
 }
